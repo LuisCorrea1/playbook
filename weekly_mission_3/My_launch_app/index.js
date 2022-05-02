d259e539ef3704ff3d125f929cf896b89bb011ab
luisSCV072 <57187479+luisSCV072@users.noreply.github.com>
2022-05-02 18:40:28 -0500

    Creación del archivo index.js

---
+++ weekly_mission_3/My_launch_app/index.js
@@ -0,0 +1,9 @@
+const MissionCommander = require('./App/missionCommander')
+
+const Carlo = new MissionCommander("Carlo")
+const Fer = new MissionCommander("Fer")
+const Rodrigo = new MissionCommander("Rodrigo")
+
+console.log(Carlo.name)
+console.log(Fer.name)
+console.log(Rodrigo.name)
